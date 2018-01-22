e7a78ad1a194529e8d488d6bbdddf7a94656e8d0
Amitav507 <amitava507@gmail.com>
2018-01-23 01:32:58 +0530

    configuration file for sms

---
+++ sms_config.py
@@ -0,0 +1,4 @@
+SSID =                                               #get the 1st 3 fields from Twilio dashboard
+AUTH_TOKEN =
+FROM_NUMBER =
+TO_NUMBER =                                              # input the number where sms needs to be sent
